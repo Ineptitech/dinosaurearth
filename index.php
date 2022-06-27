3ccd303b2bb75821b0a4327eb4f99193e5136407
Sean <sean@ineptitech.com>
2022-06-27 05:06:39 -0400

    oops

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
   </head>
   <body>
-    <model-viewer id="three" src="dino.glb" alt="Real-time satellite footage of the earth" camera-controls auto-rotate auto-rotate-delay="0" skybox-image="https://ipsyconh.sirv.com/Images/des/hdr.png?format=webp&w=2560&webp.fallback=jpg"exposure="0.6" camera-orbit="-75deg 85deg 105%"></model-viewer>
+    <model-viewer id="three" src="dinosaur.glb" alt="Real-time satellite footage of the earth" camera-controls auto-rotate auto-rotate-delay="0" skybox-image="https://ipsyconh.sirv.com/Images/des/hdr.png?format=webp&w=2560&webp.fallback=jpg"exposure="0.6" camera-orbit="-75deg 85deg 105%"></model-viewer>
     <?php echo $nav; ?>
     <?php echo $footer; ?>
   </body>
